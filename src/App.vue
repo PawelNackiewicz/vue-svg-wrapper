d90489556c8e6b25e0ba4435d067f686716d92fe
pnackiewicz <pnackiewicz@MacBook-Pro-pnackiewicz.local>
2023-06-09 19:37:15 +0200

    add missing icon

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,16 @@ import Icon from './components/Icon.vue';
 <template>
   <p>Default</p>
   <div v-for="(_, key) in defaultIcons" :key="key">
-    <Icon :name="key as keyof typeof defaultIcons" />
+    <Icon :name="key" />
   </div>
   <p>With custom color</p>
   <div v-for="(_, key) in defaultIcons" :key="key">
-    <Icon :name="key as keyof typeof defaultIcons" :color="'#ff0021'" />
+    <Icon :name="key" :color="'#ff0021'" />
   </div>
   <p>With custom size</p>
   <div v-for="(_, key) in defaultIcons" :key="key">
-    <Icon :name="key as keyof typeof defaultIcons" :size="48" />
+    <Icon :name="key" :size="48" />
   </div>
+  <p>Missing Icon</p>
+  <Icon :name="missing" />
 </template>
