5c8395b85f385408702e8258161f9d60a4dbe384
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2023-06-09 17:34:27 +0200

    init

---
+++ src/components/Icon.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { defaultIcons } from './Icons';
+import IconWrapper from './IconWrapper.vue';
+
+defineProps<{
+    name: IconName
+    classNamePath?: number
+}>()
+
+type IconName = keyof typeof defaultIcons
+
+export default defineComponent({
+    components: {
+        IconWrapper
+    }
+})
+</script>
+
+
+<template>
+    <IconWrapper {...wrapperProps}>
+        <path :d="defaultIcons[name]" fillRule="evenodd" clipRule="evenodd" className={classNamePath} />
+    </IconWrapper>
+</template>
---
+++ src/components/IconWrapper.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    // type inference enabled
+    props: {
+        name: String,
+        size: Number,
+        color: String,
+        className: String,
+        classNameG: String
+    },
+    data() {
+        return {
+            defaultSize: 20
+        }
+    }
+})
+</script>
+
+
+<template>
+    <svg :width="size" :height="size" :viewBox="`0 0 ${size} ${size}`" xmlns="http://www.w3.org/2000/svg"
+        :class="className">
+        <g :class="classNameG" :transform="`scale(${size / defaultSize})`" :fill="color">
+            {props.children}
+        </g>
+    </svg>
+</template>
